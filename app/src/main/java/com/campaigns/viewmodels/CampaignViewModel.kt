820560dcdca20b214e113002b2b8865142a460bd
alparslandev <alp.develioglu@gmail.com>
2019-04-05 22:46:16 +0300

    Service call is added

--- app/src/main/java/com/campaigns/viewmodels/CampaignViewModel.kt
+++ app/src/main/java/com/campaigns/viewmodels/CampaignViewModel.kt
@@ -5,6 +5,9 @@ import android.view.View
 import com.campaigns.R
 import com.campaigns.adapter.CampaignsAdapter
 import com.campaigns.network.Api
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class CampaignViewModel: BaseViewModel() {
@@ -12,9 +15,37 @@ class CampaignViewModel: BaseViewModel() {
     @Inject
     lateinit var api: Api
 
+    private lateinit var subscription: Disposable
+
     private var adapter = CampaignsAdapter(R.layout.item_campaign, this)
     var loading = ObservableInt(View.GONE)
     var showEmpty = ObservableInt(View.GONE)
 
-    public fun getAdapter() = adapter
+    init {
+        loadCampaigns(0)
+    }
+
+    private fun loadCampaigns(page: Int) {
+        subscription = api.getCampaigns(page)
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe { onRetrievePostListStart() }
+            .doOnTerminate { onRetrievePostListFinish() }
+            .subscribe(
+                { onRetrievePostListSuccess() },
+                { onRetrievePostListError() }
+            )
+    }
+    private fun onRetrievePostListError() { }
+
+    private fun onRetrievePostListStart() { }
+
+    private fun onRetrievePostListFinish() { }
+
+    private fun onRetrievePostListSuccess() { }
+    
+    override fun onCleared() {
+        super.onCleared()
+        subscription.dispose()
+    }
 }
