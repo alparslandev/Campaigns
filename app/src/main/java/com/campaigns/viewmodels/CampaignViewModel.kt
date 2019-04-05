2e23f3de57c53fe9c02889dfe70dcd1db7fc36fb
alparslandev <alp.develioglu@gmail.com>
2019-04-06 00:31:36 +0300

    DataBinding and RxJava dependencies are added

--- app/src/main/java/com/campaigns/viewmodels/CampaignViewModel.kt
+++ app/src/main/java/com/campaigns/viewmodels/CampaignViewModel.kt
@@ -2,6 +2,7 @@ package com.campaigns.viewmodels
 
 import androidx.databinding.ObservableInt
 import android.view.View
+import androidx.lifecycle.MutableLiveData
 import com.campaigns.R
 import com.campaigns.adapter.CampaignsAdapter
 import com.campaigns.network.Api
@@ -15,10 +16,11 @@ class CampaignViewModel: BaseViewModel() {
     @Inject
     lateinit var api: Api
 
+    private val loadingVisibility: MutableLiveData<Int> = MutableLiveData()
+
     private lateinit var subscription: Disposable
 
     private var adapter = CampaignsAdapter(R.layout.item_campaign, this)
-    var loading = ObservableInt(View.GONE)
     var showEmpty = ObservableInt(View.GONE)
 
     init {
@@ -38,12 +40,16 @@ class CampaignViewModel: BaseViewModel() {
     }
     private fun onRetrievePostListError() { }
 
-    private fun onRetrievePostListStart() { }
+    private fun onRetrievePostListStart() {
+        loadingVisibility.value = View.VISIBLE
+    }
 
-    private fun onRetrievePostListFinish() { }
+    private fun onRetrievePostListFinish() {
+        loadingVisibility.value = View.GONE
+    }
 
     private fun onRetrievePostListSuccess() { }
-    
+
     override fun onCleared() {
         super.onCleared()
         subscription.dispose()
