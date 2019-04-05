09142118e15893f6e3a88c8c7102c4ae23ec46bc
alparslandev <alp.develioglu@gmail.com>
2019-04-06 00:50:47 +0300

    MainActivity is binded

--- app/src/main/java/com/campaigns/MainActivity.kt
+++ app/src/main/java/com/campaigns/MainActivity.kt
@@ -2,11 +2,26 @@ package com.campaigns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.campaigns.databinding.ActivityMainBinding
+import com.campaigns.viewmodels.CampaignViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: CampaignViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.rvCampaigns.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+
+        viewModel = ViewModelProviders.of(this).get(CampaignViewModel::class.java)
+        binding.model = viewModel
     }
 }
