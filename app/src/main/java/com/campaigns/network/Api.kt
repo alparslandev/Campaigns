f01db8d15e9ba45a70d1da7f308b8a4f0197565c
alparslandev <alp.develioglu@gmail.com>
2019-04-05 22:04:40 +0300

    Dagger is added and Retrofit is added to it

--- app/src/main/java/com/campaigns/network/Api.kt
+++ app/src/main/java/com/campaigns/network/Api.kt
@@ -3,11 +3,12 @@ package com.campaigns.network
 import com.campaigns.model.Model
 import io.reactivex.Observable
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface Api {
     /** http://private-d190ab-campaigns19.apiary-mock.com/campaigns/20
      *  Get the list of the pots from the API
      */
-    @GET("/posts")
-    fun getCampaigns(): Observable<Model>
+    @GET("campaigns/{page}")
+    fun getCampaigns(@Path("page") page: Int): Observable<Model>
 }
